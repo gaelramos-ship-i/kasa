961cda968931ce97cae20633c8d448abf19cbc03
gaelramos-ship-i <gael.ramos@laplateforme.io>
2026-07-28 11:35:20 +0200

    finish details pages

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,9 +1,14 @@
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
-import { useState } from 'react'
 import data from "../data/logements.json"
+
 import '../styles/details.scss'
-import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
+
+import { FaArrowRight, FaArrowLeft, FaArrowDown, FaArrowUp } from "react-icons/fa";
+import { CiLocationOn } from "react-icons/ci";
+import { Collapse } from 'react-collapse'
+
+import { useState } from 'react'
 
 const Details = () => {
 
@@ -12,8 +17,12 @@ const Details = () => {
 
     const logement = data.find((item) => item.id === id)
     const images = logement.pictures
+    const tags = logement.tags
+    const equips = logement.equipments
 
     const [index, setIndex] = useState(0)
+    const [desc, setDesc] = useState(false)
+    const [equip, setEquip] = useState(false)
 
     function increment() {
         setIndex((index) => {
@@ -52,10 +61,53 @@ const Details = () => {
                 <section id='details'>
                     <div>
                         <img src={logement.pictures[index]} alt="Image Random" />
-                        <button id='prevBtn' onClick={decrement}><FaArrowLeft />
-                        </button>
-                        <button id='nextBtn' onClick={increment}><FaArrowRight />
-                        </button>
+                        <button id='prevBtn' onClick={decrement}><FaArrowLeft /></button>
+                        <button id='nextBtn' onClick={increment}><FaArrowRight /></button>
+                    </div>
+                    <div>
+                        <h1>{logement.title}</h1>
+                        <div className="location">
+                            <CiLocationOn />
+                            <p>{logement.location}</p>
+                        </div>
+                        <div className="tags">
+                            {tags.map((tag) => (
+                                <span key={tag}>{tag}</span>
+                            ))}
+                        </div>
+                        <div id='identity'>
+                            <img src={logement.host.picture} alt={`Photo de ${logement.host.name}`} />
+                            <div>
+                                <h2>{logement.host.name}</h2>
+                                <p>Hôte depuis 3 ans</p>
+                            </div>
+                        </div>
+                        <div id='collapse'>
+                            <div className='styleCollapse'>
+                                <div onClick={() => setDesc(!desc)}>
+                                    <p>Description</p>
+                                    {!desc ? <FaArrowDown /> : <FaArrowUp />}
+                                </div>
+                                <Collapse isOpened={desc}>
+                                    <div>
+                                        <p>{logement.description}</p>
+                                    </div>
+                                </Collapse>
+                            </div>
+                            <div className='styleCollapse'>
+                                <div onClick={() => setEquip(!equip)}>
+                                    <p>Équipements</p>
+                                    {!equip ? <FaArrowDown /> : <FaArrowUp />}
+                                </div>
+                                <Collapse isOpened={equip}>
+                                    <div className='grid'>
+                                        {equips.map((equip) => (
+                                            <p key={equip}>{equip}</p>
+                                        ))}
+                                    </div>
+                                </Collapse>
+                            </div>
+                        </div>
                     </div>
                 </section>
             </main>
